18e5a71ea09ddc68168adff5fcde4b5ed2144500
mora <calambaschilo@gmail.com>
2025-02-24 20:07:28 -0500

    guardar

---
+++ index/log.php
@@ -0,0 +1,34 @@
+
+<php
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log</title>
+    <link rel="stylesheet" href="log.css">
+   
+</head>
+<body>
+
+    <div class="parent">
+        <div class="div1"> </div>
+        <div class="div2"> </div>
+        <div class="div3"> </div>
+    </div>
+
+<footer>
+    <p>&copy; 2025 Mi Empresa - Todos los derechos reservados</p>
+</footer>
+
+</body>
+</html>
